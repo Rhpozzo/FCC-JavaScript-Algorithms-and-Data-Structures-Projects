39e90f7117f8dcf0de80203d3eaeda649342764d
rhpozzo@gmail.com <rhpozzo@gmail.com>
2019-05-09 22:44:43 -0400

    refactored code

--- palindromChecker.js
+++ palindromChecker.js
@@ -1,11 +1,7 @@
 function palindrome(str) {
     let a = str.toLowerCase().replace(/[^a-z0-9]/g, "") //get string turn it into lowercase and eliminate all special caracters.
-    let b = []; //hold you array to compare.
-    for(let i of a){ //push letters to compare in if statement.
-      b.push(i)
-    }
-    b = b.reverse().join('').toString() // reverse b var eliminate spaces, turn it into a string.
-   if(a===b){//compare var a and var b to see if they are the same.
+    let b = a.split('').reverse().join(''); // turn var b into a array to reverse it. join in to make into a string.
+    if(a===b){//compare var a and var b to see if they are the same.
        return true; // if same, true.
    }else{
        return false;//if not, false;
@@ -13,4 +9,4 @@ function palindrome(str) {
   }
 
 
-console.log(palindrome("A man, a plan, a canal. Panama"));
+console.log(palindrome("not a palindrome"));
