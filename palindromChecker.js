d77b3522f9e254ed1736144deb9214cce2e400e1
rhpozzo@gmail.com <rhpozzo@gmail.com>
2019-05-09 23:08:15 -0400

    updated code again with conditional operator

--- palindromChecker.js
+++ palindromChecker.js
@@ -1,11 +1,7 @@
 function palindrome(str) {
     let a = str.toLowerCase().replace(/[^a-z0-9]/g, "") //get string turn it into lowercase and eliminate all special caracters.
     let b = a.split('').reverse().join(''); // turn var b into a array to reverse it. join in to make into a string.
-    if(a===b){//compare var a and var b to see if they are the same.
-       return true; // if same, true.
-   }else{
-       return false;//if not, false;
-   }
+   return a === b ? true : false;
   }
 
 
